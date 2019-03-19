f7e2597c2c46d0646264408041bdb45c400f3ae4
morin28 <m.morie@yahoo.com>
2019-03-19 14:00:22 +0200

    Third commit

--- pythonpackage/recursion.py
+++ pythonpackage/recursion.py
@@ -22,7 +22,7 @@ def factorial(n):
     '''
     if n == 1:
         return n
-    else
+    else:
      return n * factorial(n-1)
 
 
--- pythonpackage/sorting.py
+++ pythonpackage/sorting.py
@@ -12,6 +12,22 @@ def merge_sort(items):
     '''
     Return array of items, sorted in ascending order
     '''
+    def merge(A, B):
+   	new_list = []
+        while len(A) > 0 and len(B) > 0:
+            if A[0] < B[0]:
+                new_list.append(A[0])
+            	A.pop(0)
+            else:
+                new_list.append(B[0])
+                B.pop(0)
+
+        if len(A) == 0:
+            new_list = new_list + B
+        if len(B) == 0:
+            new_list = new_list + A
+
+        return new_list
     len_i = len(items)
     # Base case. A list of size 1 is sorted.
     if len_i <= 1:
@@ -22,7 +38,7 @@ def merge_sort(items):
     a = merge_sort(items[:middle])
     b = merge_sort(items[middle:])
     # call merge_sort function on each half of list
-    return merge(a, b)
+    return merge(a,b)
 
 def quick_sort(items):
     '''
