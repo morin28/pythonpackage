ca4007e511b1202ae9dec9c780164a7e9a35d9e2
morin28 <48669647+morin28@users.noreply.github.com>
2019-03-19 14:37:33 +0200

    Update sorting.py

--- pythonpackage/sorting.py
+++ pythonpackage/sorting.py
@@ -8,42 +8,35 @@ def bubble_sort(items):
 		   		 items[j], items[j+1] = items[j+1], items[j]
     return items
 
+def merge(A, B):
+    new_list = []
+    while len(A) > 0 and len(B) > 0:
+        if A[0] < B[0]:
+            new_list.append(A[0])
+            A.pop(0)
+        else:
+            new_list.append(B[0])
+            B.pop(0)
+
+    if len(A) == 0:
+        new_list = new_list + B
+    if len(B) == 0:
+        new_list = new_list + A
+
+    return new_list
+
+
 def merge_sort(items):
-    '''
-    Return array of items, sorted in ascending order
-    '''
-    def merge(A, B):
-        new_list = []
-        while len(A) > 0 and len(B) > 0:
-            if A[0] < B[0]:
-                new_list.append(A[0])
-                A.pop(0)
-        new_list = []
-        while len(A) > 0 and len(B) > 0:
-            if A[0] < B[0]:
-                new_list.append(A[0])
-                A.pop(0)
-            else:
-                new_list.append(B[0])
-                B.pop(0)
 
-        if len(A) == 0:
-            new_list = new_list + B
-        if len(B) == 0:
-            new_list = new_list + A
-
-        return new_list
     len_i = len(items)
-    # Base case. A list of size 1 is sorted.
-    if len_i <= 1:
+    if len_i == 1:
         return items
-    # Recursive case. Find midpoint of list
-    middle = int(len_i / 2)
-    # divide list into two halves
-    a = merge_sort(items[:middle])
-    b = merge_sort(items[middle:])
-    # call merge_sort function on each half of list
-    return merge(a,b)
+
+    mid_point = int(len_i / 2)
+    i1 = merge_sort(items[:mid_point])
+    i2 = merge_sort(items[mid_point:])
+
+    return merge(i1, i2)
 
 def quick_sort(items, index=-1):
     len_i = len(items)
